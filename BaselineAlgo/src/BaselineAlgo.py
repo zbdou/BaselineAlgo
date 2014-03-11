6bcea14d151e865eac28183725e7159a41a1d05d
zbdou <zhibin.dou@gmail.com>
2014-03-11 23:24:01 +0800

    node mapping is done, recursive version!

--- BaselineAlgo/src/BaselineAlgo.py
+++ BaselineAlgo/src/BaselineAlgo.py
@@ -1,3 +1,4 @@
+#-*- coding: UTF-8 -*-
 '''
 Created on 3/10/2014
 
@@ -13,9 +14,12 @@ def build_virtual_network():
     test topo:
         see the visio graph.
     """
-    g = nx.Graph(done = deque())
+    g = nx.Graph(not_done = deque())
     g.add_nodes_from(range(1, 4))
     g.add_edges_from([(1, 2), (2, 3), (1, 3)])
+    g.graph['not_done'] = g.nodes()
+    
+    # print "not_done", g.graph['not_done']
     
     # b(ev(i, j) )
     g[1][2]['bw'] = 30
@@ -31,11 +35,12 @@ def build_virtual_network():
         g.node[i]['ns'] = None
         
     # dump topology
+    """
     print "------------------------------------------------"
     print g.graph
     print g.nodes(data = True)
     print g.edges(data = True)
-    
+    """
     return g
 
 def build_substrate_network():
@@ -60,21 +65,86 @@ def build_substrate_network():
     cpus = [100, 50, 30, 80, 20]
     for i in xrange(1, len(cpus) + 1):
         g.node[i]['cpu'] = cpus[i-1]
-        g.node[i]['nv'] = deque()
         
     # dump topology
+    """
     print "------------------------------------------------"
     print g.graph
     print g.nodes(data = True)
     print g.edges(data = True)
-    
+    """
     return g
 
-
+class BaseLineAlgo:
+    """Virtual Network Mapping, baseline algorithm"""
+    def __init__(self, gv, gs):
+        self.gv = gv
+        self.gs = gs
+        assert self.gv, self.gs
+        
+    def run(self, all_solutions = True,  verbose_output = True):
+        """
+        if all_solutions == False, then return if find the first solution
+        if verbose_output == True, then output 1) the total number of iterations, 2) output the solution.
+        
+        return: all solution if all_solutions = True, otherwise, return the first or None, if no feasible solution found.
+        """
+        virtual_nodes = deque(self.gv.nodes())
+          
+        # find the total number of iterations
+        total_iterations = reduce(lambda x, y: x+y, map(lambda x: len(self.gv.node[x]['candidates']), virtual_nodes))
+        print "The total number of iterations is: {total_iterations}".format(total_iterations=total_iterations)
+        
+        self._run()
+        
+    def _run(self):
+        '''recursive function'''
+        # 0. not_done is empty
+        if not self.gv.graph['not_done']:
+            # print self.gs.nodes(data=True)
+            print self.gv.nodes(data=True)
+            return
+        # 1. get the first nv that is in not_done
+        nv = self.gv.graph['not_done'].pop(0)
+        
+        # 2. get the map(nv)
+        candidates = self.gv.node[nv]['candidates']
+        candidates_mapping = dict(zip(candidates, map(lambda x: self.gs.node[x]['cpu'], candidates)))
+        
+        # print candidates
+        # print candidates_mapping
+        desc_candi_list = sorted(candidates_mapping.iteritems(), key = lambda d:d[1], reverse = True)
+        #print desc_candi_list
+        assert desc_candi_list
+        
+        nv_cpu = self.gv.node[nv]['cpu']
+        
+        # step 3.        
+        for ns, ns_cpu in desc_candi_list:
+            # step 4.
+            if nv_cpu > ns_cpu: 
+                self.gv.graph['not_done'].insert(0, nv);
+                return
+            
+            # step 5. update the resource allocation database
+            self.gs.node[ns]['cpu'] -= nv_cpu
+            self.gv.node[nv]['ns'] = ns
+            
+            # step 6. 
+            self._run()
+            
+            # step 7. restore the resource allocation database
+            self.gs.node[ns]['cpu'] += nv_cpu
+            self.gv.node[nv]['ns'] = None
+        
+        self.gv.graph['not_done'].insert(0, nv);                             
+        return
 
 if __name__ == '__main__':
+    Gv = build_virtual_network()    
     Gs = build_substrate_network()
-    Gv = build_virtual_network()
     
+    BaseLineAlgo(Gv, Gs).run()
+    print Gv.graph
     
 
