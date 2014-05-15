09105d955cd72856c31841eab212919fc9b23e87
zbdou <zhibin.dou@gmail.com>
2014-05-15 22:26:01 +0800

    1. 加入了对多个虚拟节点映射到同一个物理节点的支持 2. 针对多路径、单路径的KSP进行了参数优化

--- BaselineAlgo/src/BaselineAlgo.py
+++ BaselineAlgo/src/BaselineAlgo.py
@@ -23,13 +23,13 @@ def build_virtual_network():
     g.graph['not_done'] = g.nodes()
 
     # assign edge bandwidth (link resource)
-    g[1][2]['bw'] = 90
-    g[1][3]['bw'] = 50
-    g[2][3]['bw'] = 80
+    g[1][2]['bw'] = 30
+    g[1][3]['bw'] = 26
+    g[2][3]['bw'] = 50
     
     # assign node computation resource
     cpus = [20, 60, 30]
-    map_nv = [[5, 10, 15], [1, 4, 9], [2, 3, 6, 7]]
+    map_nv = [[1,3,5,7,9,11], [2,4,6,8,10,12,14], [3,6,9,12,15,18]]
     for i in xrange(1, len(cpus) + 1):
         g.node[i]['cpu'] = cpus[i-1]
         g.node[i]['candidates'] = map_nv[i-1]
@@ -37,9 +37,11 @@ def build_virtual_network():
         
     # dump topology
     pos = nx.shell_layout(g)
-    nx.draw_networkx_nodes(g, pos)
+    nx.draw_networkx_nodes(g, pos, node_size=1500)
     nx.draw_networkx_edges(g, pos)
-    nx.draw_networkx_labels(g, pos)
+    
+    node_labels = {n: "{0}({1})".format(n, g.node[n]['cpu']) for n in g.nodes()}    
+    nx.draw_networkx_labels(g, pos,labels=node_labels)
     
     edge_labels = {(u,v): bw['bw'] for u,v,bw in g.edges(data=True)}
     nx.draw_networkx_edge_labels(g, pos, edge_labels, label_pos=0.3)
@@ -67,6 +69,8 @@ def delay_generator(dmin = 100, dmax = 100, times10 = True):
 def build_substrate_network(nodes_no = 10, max_edge_degree = 1):
     """
     build the substrate network which is of circle-like.
+    @nodes_no 物理网络的节点总数
+    @max_edge_degree 每个节点的最大度数，（不包含与前驱、后继节点相连的两条链路）
     """
     MAX_NODES_NO = nodes_no + 1
     NO_BASE = MAX_NODES_NO - 1
@@ -80,6 +84,8 @@ def build_substrate_network(nodes_no = 10, max_edge_degree = 1):
     for i in xrange(1, len(cpus) + 1):
         g.node[i]['cpu'] = cpus[i-1]
   
+    
+
     # build edge and weight (bandwidth & delay)
     for i in xrange(1, MAX_NODES_NO):
         # excluding the direct (pre, i), (i, next) links
@@ -99,13 +105,14 @@ def build_substrate_network(nodes_no = 10, max_edge_degree = 1):
             dst_no -= 1
 
     pos = nx.circular_layout(g, scale=2)
-    
-    nx.draw_networkx_nodes(g, pos)
+
+    node_labels = {n: "{0}({1})".format(n, g.node[n]['cpu']) for n in g.nodes()}
+    nx.draw_networkx_nodes(g, pos, node_sizes=5000)
     nx.draw_networkx_edges(g, pos)
-    nx.draw_networkx_labels(g, pos)
+    nx.draw_networkx_labels(g, pos, labels=node_labels, font_size=5)
     
     edge_labels = {(u,v): bw['bw'] for u, v, bw in g.edges(data=True)}
-    nx.draw_networkx_edge_labels(g, pos, edge_labels, label_pos=0.3)
+    nx.draw_networkx_edge_labels(g, pos, edge_labels, label_pos=0.3, font_size=8)
     
     plt.axis('off')       
     plt.savefig("substrate", transparent=True, dpi=300)
@@ -122,39 +129,40 @@ def graph2KSPGraph(g):
     map(lambda x: g2.add_node(x), g.nodes_iter())
     map(lambda x: add_edge(x[0], x[1], x[2]['dy']), g.edges_iter(data=True))
         
-    g2.set_name("aaa")
-    g2.export()
+    #g2.set_name("aaa")
+    #g2.export()
     return g2
         
 class BaselineAlgo:
     MAX_BW = 1e5
     """Virtual Network Mapping, baseline algorithm"""
-    def __init__(self, gv, gs, all_solutions = True, verbose = True, max_split_no = 1):
+    def __init__(self, gv, gs, all_solutions = True, max_split_no = 1, max_k = 5):
         """
-        @isBW_SP 资源分配方式，按照链路带宽，是否允许split path
+        @all_solutions: 是否求解所有的节点映射方案
+        @max_split_no: 是否允许路径分割映射，= 1 表示不允许，> 1 表示一条虚拟路径可以映射到 @max_split_no 条物理路径
+        @max_k: 仅在 @max_split_no == 1 时有意义，表示按照带宽作为物理路径的cost时，搜索空间，
+                                       即最多搜索 @max_k 条物理cost最短的路径（KSP，K Shortest Paths）
         """
         self.gv = gv
         self.gs = gs
         self.all_solutions = all_solutions
-        self.verbose = verbose
         self.solution_found = False
         self.solution_counter = 0
         self.max_split_no = max_split_no
+        self.max_k = max_k
         assert self.gv, self.gs
         
     def run(self):
         """
         if all_solutions == False, then return if find the first solution
-        if verbose_output == True, then output 1) the total number of iterations, 2) output the solution.
-        
         return: all solution if all_solutions = True, otherwise, return the first or None, if no feasible solution found.
         """
         virtual_nodes = deque(self.gv.nodes())
           
         # find the total number of iterations
         total_iterations = reduce(lambda x, y: x*y, map(lambda x: len(self.gv.node[x]['candidates']), virtual_nodes))
-        print "The total number of iterations is: {total_iterations}".format(total_iterations=total_iterations)
-        print "Find all solutions: {0}, verbose output: {1}".format(self.all_solutions, self.verbose)
+        print "The total number of iterations is: {0}".format(total_iterations)
+        print "Find all solutions: {0}".format(self.all_solutions)
         
         self._run()
     
@@ -165,27 +173,37 @@ class BaselineAlgo:
         @gv virtual network
         @gs substrate network
         
-        @return tuple (path, path bandwidth) of the shortest path.
-                else None
+        @return list of paths:
+            [{'path': [a,b,c], 'cost': cost, 'bw': path bandwidth}, {}, {}, ...]
         """
         # 1. get the physical psrc, pdst
         psrc, pdst = gv.node[vsrc]['ns'], gv.node[vdst]['ns']
         
         if nx.has_path(gs, psrc, pdst) == False: return None
-        path = nx.shortest_path(gs, psrc, pdst, weight="dy")
         
-        max_k = 3
-        gs2 = graph2KSPGraph(gs)
-        path2 = algorithms.ksp_yen(gs2, psrc, pdst, max_k)
-        print path2
+        # check the abnormal case: psrc = pdst
+        if psrc == pdst:
+            return [{'path':[psrc, pdst], 'cost': 0, 'bw': self.MAX_BW}]
         
+        if self.max_split_no != 1: 
+            max_k = 1
+        else:
+            max_k = self.max_k
+            
+        paths= algorithms.ksp_yen(graph2KSPGraph(gs), psrc, pdst, max_k)
+
         # calculate the path bandwidth based on the link bandwidth
-        bws = map(lambda src, dst: gs[src][dst]['bw'], path[:-1], path[1:])
-        return (path, min(bws))
+        for item in paths:
+            path = item['path']
+            bws = map(lambda src, dst: gs[src][dst]['bw'], path[:-1], path[1:])
+            item['bw'] = min(bws)
+        
+        #print "path2 = ", paths    
+        return paths
 
     def dump_virtual_path(self, gs, gv, src, dst, path):
         # for each physical path
-        g = nx.MultiGraph()
+        g = nx.Graph()
                  
         nodes = list()
         map(lambda x: nodes.extend(x[0]), gv[src][dst]['allocated_path'])
@@ -204,11 +222,11 @@ class BaselineAlgo:
         
         nx.draw_networkx_labels(g, pos)
         name = "solution({0})_{1}-{2}".format(self.solution_counter, src, dst)
+        
         plt.axis('off')
         plt.legend(loc='best')
         plt.tight_layout(0)
         plt.savefig(name, transparent=True)
-        
         plt.clf()
 
     def _run(self):
@@ -232,17 +250,28 @@ class BaselineAlgo:
                 done = False
                 while not done:
                     if max_split == 0:
+                        # FIXME: 不是最优解，最优解的搜索空间是： Cn^m,其中 n = max_k，m = max_split_no
                         print "max_split = 0"
                         failed = True
                         break
                     
                     sp = self._virtual_path_mapping(src, dst, gv, gs)
-                    if sp is None or (sp[1] < vbw and self.max_split_no == 1):
-                        print "sp is None or (no split and bw is not enough"
+                    
+                    if sp is None: 
+                        print "sp is None"
                         failed = True
                         break
                     
-                    (path, pbw) = sp
+                    path, delay, pbw = sp[0]['path'], sp[0]['cost'], sp[0]['bw'] 
+                    if self.max_split_no == 1:
+                        allowed_path = [(item['path'], item['cost'], item['bw']) for item in sp if item['bw'] >= vbw]
+                        if len(allowed_path) == 0: 
+                            print "no split and bw is not enough"
+                            failed = True
+                            break
+                        else:
+                            (path, delay, pbw)= allowed_path[0]
+                    
                     # 对该虚拟路径(src, dst)依次分配物理路径和带宽，并更新资源视图
                     bwmin = min(vbw, pbw)
                     vbw -= bwmin
@@ -250,6 +279,7 @@ class BaselineAlgo:
                     # update gs resource view
                     gv[src][dst]['allocated_path'].append((path, bwmin))                        
                     for link_src, link_dst in zip(path[:-1], path[1:]):
+                        if link_src == link_dst: continue
                         gs[link_src][link_dst]['bw'] -= bwmin
                         if gs[link_src][link_dst]['bw'] == 0:
                             gs.remove_edge(link_src, link_dst)
@@ -320,8 +350,8 @@ def remove_images(image_dir):
 def main():
     remove_images("./")
     Gv = build_virtual_network()    
-    Gs = build_substrate_network(15, 1)
-    BaselineAlgo(Gv, Gs, all_solutions = True, max_split_no = 2).run()
+    Gs = build_substrate_network(50, 1)
+    BaselineAlgo(Gv, Gs, all_solutions = True, max_split_no = 2, max_k = 50).run()
 
 if __name__ == '__main__':
     main()
